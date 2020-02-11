dbe9fbe9f0150f0bec46822c541bf7434caf09ad
simple0426 <perfect_0426@qq.com>
2020-02-11 21:49:37 +0800

    变更注释

--- build-prod.sh
+++ build-prod.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 
-#内部端口
+# 服务端口
 targetPort=8080
-#镜像版本号
+# 镜像版本号
 vendor=1.0.0
-#项目名
+# 项目名称
 projectName=spring-hello-demo
 
-# 打包
+# 软件打包
 cd $WORKSPACE
 mvn clean package -D skipTests
 
-#删除镜像下所有容器
+# 删除基于镜像的所有容器
 if [ $(docker ps -qf "name=$projectName") ];then 
     docker rm -f $projectName
 fi
 
-#删除旧镜像
+# 删除旧镜像
 if [ $(docker images -qf "reference=$projectName:$vendor") ];then 
     docker rmi -f $projectName:$vendor
 fi
  
-#创建镜像
+# 创建镜像
 docker build -t $projectName:$vendor .
  
-#启动镜像生成容器
+# 基于镜像启动容器
 docker run --name $projectName -d -p $targetPort:$targetPort $projectName:$vendor
